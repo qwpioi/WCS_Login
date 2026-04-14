597c354cf77faff7d8ac1c750c868829c6c7db3b
qwpioi <2217188430@qq.com>
2026-04-14 14:39:53 +0800

    [界面] 设置登录窗体基础属性 - 窗体大小：800x550 - 禁用最大化/最小化 - 启动居中显示

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(778, 494);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "登录";
+            this.ResumeLayout(false);
+
         }
 
         #endregion

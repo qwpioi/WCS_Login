c988b485de50d28ac299f8bb5fd14723881cb2d9
qwpioi <2217188430@qq.com>
2026-04-14 15:44:23 +0800

    [界面] 添加系统模式下拉框 - LabelControl: 系统模式标签 - ComboBoxEdit: 模式下拉框 - 选项：运行模式、配置模式 - 默认值：运行模式

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,7 +30,10 @@
         {
             this.lblTitle = new DevExpress.XtraEditors.LabelControl();
             this.pictureEdit1 = new DevExpress.XtraEditors.PictureEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.cmbMode = new DevExpress.XtraEditors.ComboBoxEdit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbMode.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // lblTitle
@@ -46,18 +49,43 @@
             // pictureEdit1
             // 
             this.pictureEdit1.EditValue = global::WCS_Login.Properties.Resources.user_icon;
-            this.pictureEdit1.Location = new System.Drawing.Point(22, 191);
+            this.pictureEdit1.Location = new System.Drawing.Point(48, 191);
             this.pictureEdit1.Name = "pictureEdit1";
             this.pictureEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
             this.pictureEdit1.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
             this.pictureEdit1.Size = new System.Drawing.Size(187, 181);
             this.pictureEdit1.TabIndex = 1;
             // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(340, 191);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(90, 24);
+            this.labelControl1.TabIndex = 2;
+            this.labelControl1.Text = "系统模式：";
+            // 
+            // cmbMode
+            // 
+            this.cmbMode.EditValue = "运行模式";
+            this.cmbMode.Location = new System.Drawing.Point(437, 191);
+            this.cmbMode.Name = "cmbMode";
+            this.cmbMode.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.cmbMode.Properties.Items.AddRange(new object[] {
+            "运行模式",
+            "配置模式"});
+            this.cmbMode.Size = new System.Drawing.Size(150, 28);
+            this.cmbMode.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(778, 494);
+            this.Controls.Add(this.cmbMode);
+            this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.pictureEdit1);
             this.Controls.Add(this.lblTitle);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -67,6 +95,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "登录";
             ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbMode.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -76,6 +105,8 @@
 
         private DevExpress.XtraEditors.LabelControl lblTitle;
         private DevExpress.XtraEditors.PictureEdit pictureEdit1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.ComboBoxEdit cmbMode;
     }
 }
 
